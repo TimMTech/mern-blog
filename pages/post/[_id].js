5d700bb9487e1d8022e8a4bea80add36ba5d6b99
TimMTech <tmukhamedov97@gmail.com>
2022-09-12 16:38:44 -0400

    changed fetch urls to site domain name.

--- pages/post/[_id].js
+++ pages/post/[_id].js
@@ -12,7 +12,7 @@ const post = ({post}) => {
 export default post
 
 export const getStaticPaths = async() => {
-    const res = await fetch("http://localhost:3000/api/post")
+    const res = await fetch("http://etechblog.io/api/post")
     const data = await res.json()
 
     const paths = data.map((post) => {
@@ -29,7 +29,7 @@ export const getStaticPaths = async() => {
 export const getStaticProps = async(context) => {
     const _id = context.params._id;
 
-    const res = await fetch(`http://localhost:3000/api/post/${_id}`)
+    const res = await fetch(`http://etechblog.io/api/post/${_id}`)
     const data = await res.json()
 
     return {

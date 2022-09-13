f3636cbc7f90123099cf64a178f208d7ade6ed94
TimMTech <tmukhamedov97@gmail.com>
2022-09-13 19:16:29 -0400

    updating env again.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -34,7 +34,7 @@ export default NextAuth({
           username: credentials.username,
         });
         if (!user) {
-          console.log(user)
+          
           return null;
         }
         const isPasswordValid = await bcrypt.compare(
